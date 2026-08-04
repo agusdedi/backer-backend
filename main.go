78eb8b0dafdb8576b9750b4855a2fce63fdea2f8
agusdedi <dedipriyana16@gmail.com>
2026-08-04 14:43:21 +0800

    Add admin CMS with user list and avatar fallback

--- main.go
+++ main.go
@@ -13,11 +13,15 @@ import (
 	"log"
 	"net/http"
 	"os"
+	"path/filepath"
 	"strings"
 	"time"
 
+	webHandler "backer/web/handler"
+
 	"github.com/dgrijalva/jwt-go"
 	"github.com/gin-contrib/cors"
+	"github.com/gin-contrib/multitemplate"
 	"github.com/gin-gonic/gin"
 	"github.com/joho/godotenv"
 	"gorm.io/driver/mysql"
@@ -57,6 +61,8 @@ func main() {
 	campaignHandler := handler.NewCampaignHandler(campaignService)
 	transactionHandler := handler.NewTransactionHandler(transactionService)
 
+	userWebHandler := webHandler.NewUserHandler(userService)
+
 	// Router
 	router := gin.Default()
 
@@ -74,7 +80,12 @@ func main() {
 		MaxAge:           12 * time.Hour,
 	}))
 
+	router.HTMLRender = loadTemplates("./web/templates")
+
 	router.Static("/images", "./images")
+	router.Static("/css", "./web/assets/css/")
+	router.Static("/js", "./web/assets/js/")
+	router.Static("/webfonts", "./web/assets/webfonts/")
 
 	api := router.Group("/api/v1")
 
@@ -97,6 +108,8 @@ func main() {
 	api.GET("/transactions", authMiddleware(authService, userService), transactionHandler.GetUserTransactions)
 	api.POST("/transactions", authMiddleware(authService, userService), transactionHandler.CreateTransaction)
 	api.POST("/transactions/notification", transactionHandler.GetNotification)
+
+	router.GET("/users", userWebHandler.Index)
 
 	router.Run(":8080")
 }
@@ -143,4 +156,27 @@ func authMiddleware(authService auth.Service, userService user.Service) gin.Hand
 
 		c.Set("currentUser", user)
 	}
+}
+
+func loadTemplates(templatesDir string) multitemplate.Renderer {
+	r := multitemplate.NewRenderer()
+
+	layouts, err := filepath.Glob(templatesDir + "/layouts/*")
+	if err != nil {
+		panic(err.Error())
+	}
+
+	includes, err := filepath.Glob(templatesDir + "/**/*")
+	if err != nil {
+		panic(err.Error())
+	}
+
+	// Generate our templates map from our layouts/ and includes/ directories
+	for _, include := range includes {
+		layoutCopy := make([]string, len(layouts))
+		copy(layoutCopy, layouts)
+		files := append(layoutCopy, include)
+		r.AddFromFiles(filepath.Base(include), files...)
+	}
+	return r
 }
